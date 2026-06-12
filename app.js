2ba6ee284a62a089292c932c34ac323d2b12b982
engrwm <wm900161@gmail.com>
2026-06-12 11:24:35 +0100

    update

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ id:1,
 name:"Tesla Model S",
 price:"$78,000",
 range:"652 km",
-speed:"0–100 in 2.1 sec",
-color:"Ultra Red",
+speed:"2.1 sec",
+color:"white",
+
 image:
-"https://images.unsplash.com/photo-1560958089"
+"https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?auto=format&fit=crop&w=1200&q=80"
 },
 
 {
@@ -16,10 +17,11 @@ id:2,
 name:"Tesla Model 3",
 price:"$48,000",
 range:"513 km",
-speed:"0–100 in 4.4 sec",
-color:"Grey",
+speed:"4.4 sec",
+color:"ultra red",
+
 image:
-"https://images.unsplash.com/photo-1553440569"
+"https://images.unsplash.com/photo-1553440569-bcc63803a83d?auto=format&fit=crop&w=1200&q=80"
 },
 
 {
@@ -27,10 +29,11 @@ id:3,
 name:"Tesla Model X",
 price:"$92,000",
 range:"560 km",
-speed:"0–100 in 2.6 sec",
-color:"White",
+speed:"2.6 sec",
+color:"Black",
+
 image:
-"https://images.unsplash.com/photo-1502877338535"
+"https://images.unsplash.com/photo-1503376780353-7e6692767b70?auto=format&fit=crop&w=1200&q=80"
 },
 
 {
@@ -38,10 +41,11 @@ id:4,
 name:"Tesla Model Y",
 price:"$56,000",
 range:"533 km",
-speed:"0–100 in 3.5 sec",
-color:"Black",
+speed:"3.5 sec",
+color:"Blue",
+
 image:
-"https://images.unsplash.com/photo-1492144534655"
+"https://images.unsplash.com/photo-1502877338535-766e1452684a?auto=format&fit=crop&w=1200&q=80"
 }
 
 ];
